33fe588f85eb10b71686725926a72646d9a69e7c
monika-stempniak <mstempniak@gmail.com>
2019-05-02 19:15:58 +0200

    Move App.js to components folder

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+import ProductsList from '../ProductsList/ProductsList'
+
+function App() {
+  return (
+    <div>
+      <ProductsList />
+    </div>
+  );
+}
+
+export default App;
