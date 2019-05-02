68494f16ff327ce56cbffaa9f7418dfe6461519c
monika-stempniak <mstempniak@gmail.com>
2019-05-02 19:15:23 +0200

    Create ProductsList and ProductItem component

---
+++ src/components/ProductItem/ProductItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ProductItem.module.scss';
+
+class ProductItem extends React.Component {
+  render() {
+    return (
+      <div>
+        ProductItem
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ProductsList.module.scss';
+
+class ProductsList extends React.Component {
+  render() {
+    return (
+      <div>
+        ProductsList
+      </div>
+    );
+  }
+}
+
+export default ProductsList;
