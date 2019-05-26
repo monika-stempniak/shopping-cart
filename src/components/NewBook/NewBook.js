c5d9cd4d40fd3aa94a5a36f387e539836f8e103f
monika-stempniak <mstempniak@gmail.com>
2019-05-26 21:06:42 +0200

    Create NewBook page

---
+++ src/components/NewBook/NewBook.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import styles from './NewBook.module.scss';
+
+class NewBook extends React.Component {
+  render() {
+    // const { books, updateBooks, updateCart, userId, checkUserID } = this.props;
+
+    // if (!books) {
+    //   return <p>Loading...</p>;
+    // }
+
+    return (
+      <div>New Book</div>
+    );
+  }
+}
+
+export default NewBook;
