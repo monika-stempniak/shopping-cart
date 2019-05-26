7099ff687b20ae4e461124c71d592565b414c861
monika-stempniak <mstempniak@gmail.com>
2019-05-26 21:05:06 +0200

    Rename ProductsList as Books and update

---
+++ src/components/Books/Books.js
@@ -0,0 +1,63 @@
+import Axios from 'axios';
+import React from 'react';
+
+import { API_URL } from '../../constans';
+import Book from "../Book/Book";
+
+import styles from './Books.module.scss';
+
+class Books extends React.Component {
+  state = {
+    books: null,
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  getBooks = async () => {
+    try {
+      const response = await Axios.get(`${API_URL}/books`);
+      const data = await response.data;
+
+      this.setState({
+        books: data,
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  render() {
+    const { books } = this.state;
+
+    if (!books) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <main className={styles.wrapper}>
+        <div className="container">
+          <h1 className={styles.title}>Books</h1>
+          <div className="row">
+            {
+              books.map((book) => (
+                <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={book._id}>
+                  <Book
+                    {...book}
+                    // updateBooks={updateBooks}
+                    // updateCart={updateCart}
+                    // userId={userId}
+                    // checkUserID={checkUserID}
+                  />
+                </div>
+              ))
+            }
+        </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Books;
