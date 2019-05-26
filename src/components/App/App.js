c3c0264da832003a1f0d61b6daf94303195e5c89
monika-stempniak <mstempniak@gmail.com>
2019-05-26 21:01:38 +0200

    Add basic App routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,100 +1,29 @@
 import React from 'react';
-import Axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-import styles from './App.module.scss';
-
-import ProductsList from '../ProductsList/ProductsList'
-import Cart from '../Cart/Cart'
-const URL = require('../../constans');
+import Books from '../Books/Books';
+import Cart from '../Cart/Cart';
+import NewBook from '../NewBook/NewBook';
+import Header from '../Header/Header';
+import NotFound from '../NotFound/NotFound';
 
 class App extends React.Component {
-  state = {
-    books: null,
-    cart: null,
-    userId: null,
-  }
-
-  getBooks = () => {
-    Axios.get(`${URL}/books`)
-      .then(response => {
-        this.setState({
-          books: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log(error.message);
-      })
-  }
-
-  getCart = (userId) => {
-    Axios.get(`${URL}/cart/${userId}`)
-      .then(response => {
-        this.setState({
-          cart: response.data,
-        })
-      })
-      .catch((error) => {
-        console.log(error.message);
-      })
-  }
-
-  componentDidMount() {
-    this.getBooks();
-    this.getCart();
-  }
-
-  updateBooks = () => {
-    this.getBooks();
-  }
-
-  updateCart = (userId) => {
-    this.getCart(userId);
-  }
-
-  checkUser = (userId) => {
-    const id = +userId;
-    if (id && typeof id === 'number') {
-      this.setState({ userId: id })
-      this.updateCart(userId);
-    } else {
-      const passedId = prompt('Invalid User ID, please try again');
-      this.checkUserID(passedId);
-    }
-  }
-
-  checkUserID = (userId) => {
-    this.checkUser(userId);
-  }
 
   render() {
-    const { books, cart, userId } = this.state;
-
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className={styles.wrapper}>
-            {
-              userId
-              ? <span className={styles.loggedIn}>Logged in: ID {userId}</span>
-              : <span className={styles.loggedIn}>Logged out</span>
-            }
-              <ProductsList 
-                books={books} 
-                updateCart={this.updateCart} 
-                updateBooks={this.updateBooks} 
-                userId={userId}
-                checkUserID={this.checkUserID}
-              />
-              <Cart 
-                cart={cart} 
-                updateBooks={this.updateBooks} 
-                userId={userId}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Books} />
+          <Route exact path='/cart' component={Cart} />
+          <Route exact path='/new' component={NewBook} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     );
   }
 }
