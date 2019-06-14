e46df96e8604efff7d145a014278982b5c60c3be
monika-stempniak <mstempniak@gmail.com>
2019-06-14 21:17:55 +0200

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,25 +6,27 @@ import styles from './Header.module.scss';
 const Header = ({ user, children }) => {
 
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink exact activeClassName={styles.active} to="/">Books</NavLink>
-        </li>
-        {
-          user && user.login === "admin" 
-          ? (
-            <li>
-            <NavLink activeClassName={styles.active} to="/new">Add Book</NavLink>
-            </li>
-          ) : (
-            <li>
-              <NavLink activeClassName={styles.active} to="/cart">Cart</NavLink>
+    <div className={styles.wrapper}>
+      <nav className={styles.navbar}>
+        {children}
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>
+            <NavLink exact activeClassName={styles.active} className={styles.navLink} to="/">Books</NavLink>
           </li>
-          )
-        }
-      </ul>
-      {children}
+          {
+            user && user.login === "admin" 
+            ? (
+              <li className={styles.navItem}>
+              <NavLink activeClassName={styles.active} className={styles.navLink} to="/new">Add Book</NavLink>
+              </li>
+            ) : (
+              <li className={styles.navItem}>
+                <NavLink activeClassName={styles.active} className={styles.navLink} to="/cart">Cart</NavLink>
+            </li>
+            )
+          }
+        </ul>
+      </nav>
     </div>
   );
 }
